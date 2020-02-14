d58d5cc9b5cb763cb30197cfbf0ab8a23a1e09e1
vagrant <vagrant@vagrant.vm>
2020-02-14 02:05:06 +0000

    added html and created databases

--- model.py
+++ model.py
@@ -4,8 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = "SECRETKEY"
 
-DB_URI = "postgresql:///voting"
-
 db = SQLAlchemy()
 
 #################################################################################
@@ -15,7 +13,7 @@ db = SQLAlchemy()
 class PollingCenter(db.Model):
     """Table containing polling center's information"""
 
-    __tablename__ = "polling-centers"
+    __tablename__ = "pollingcenters"
 
     polling_id = db.Column(db.Integer,
                             primary_key=True,
@@ -24,12 +22,13 @@ class PollingCenter(db.Model):
     lat = db.Column(db.Integer, nullable=False)
     lng = db.Column(db.Integer, nullable=False)
     hours_of_operation = db.Column(db.String(50), nullable=False)
-    
 
     def __repr__(self):
         """Human readable representation of data from PollingCenter table"""
 
-        return f"<Polling Center polling_id={self.polling_id} address={self.address} hours_of_operation={self.hours_of_operation}"
+        return f"""<Polling Center polling_id={self.polling_id} 
+                                address={self.address} 
+                                hours_of_operation={self.hours_of_operation}>"""
 
 class Comment(db.Model):
     """Table containing User's comments for each Polling Center"""
@@ -39,14 +38,24 @@ class Comment(db.Model):
     comment_id = db.Column(db.Integer,
                             primary_key=True,
                             autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), index=True)
-    polling_id = db.Column(db.Integer, db.ForeignKey('polling-centers.polling_id'), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    polling_id = db.Column(db.Integer, db.ForeignKey('pollingcenters.polling_id'), nullable=False)
     comment = db.Column(db.String(140), nullable=False)
+
+    #Defining relationship between Comment and Polling Center tables
+    polling_center = db.relationship("PollingCenter",
+                                    backref=db.backref("comments", order_by=comment_id))
+
+    #Defining relationship between Comment and User table
+    user = db.relationship("User",
+                            backref=db.backref("comments", order_by=comment_id))
 
     def __repr__(self):
         """Human readable representation of data from Comments table"""
 
-        return f"<Comment comment_id={self.comment_id} user_id={self.user_id} polling_id={self.polling_id}"
+        return f"""<Comment comment_id={self.comment_id} 
+                            user_id={self.user_id} 
+                            polling_id={self.polling_id}>"""
 
 class User(db.Model):
     """Table containing User's profile information"""
@@ -57,19 +66,24 @@ class User(db.Model):
                         primary_key=True,
                         autoincrement=True)
     
-    party_id = db.Column(db.Integer, db.ForeignKey('parties.party_id'), index=True)
-    street = db.Column(db.String(100), nullable=False)
-    city = db.Column(db.String(100), nullable=False)
-    state = db.Column(db.String(3), nullable=False)
-    zipcode = db.Column(db.Integer, nullable=False)
-    fname = db.Column(db.String(20), nullable=False)
+    party_id = db.Column(db.Integer, db.ForeignKey('parties.party_id'), nullable=False)
+    address = db.Column(db.String(300), nullable=False)
+    fname = db.Column(db.String(25), nullable=False)
     lname = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     lat = db.Column(db.Integer, nullable=False)
     lng = db.Column(db.Integer, nullable=False)
 
+    #Defining relationship between User and Comment table
+    comment = db.relationship("Comment",
+                            backref=db.backref("users", order_by=user_id))
+    
+    #Defining relationship between User and Parties table
+    parties = db.relationship("Parties",
+                            backref=db.backref("users", order_by=user_id))
+
     def __repr__(self):
-        return f"<User user_id={self.user_id} party_id={self.party_id}"
+        return f"""<User user_id={self.user_id} party_id={self.party_id}>"""
 
 class Parties(db.Model):
     """Table containing each political party"""
@@ -82,8 +96,12 @@ class Parties(db.Model):
     
     political_party = db.Column(db.String(100), nullable=False)
 
+    #Defining relationship between Parties and Political Candidates tables
+    candidates = db.relationship("PoliticalCandidates",
+                                backref=db.backref("parties", order_by=party_id))
+
     def __repr__(self):
-        return f"<Parties party_id={self.party_id} political_party={self.political_party}"
+        return f"<Parties party_id={self.party_id} political_party={self.political_party}>"
 
 class PoliticalCandidates(db.Model):
     """Table containing each political candidates information"""
@@ -91,19 +109,29 @@ class PoliticalCandidates(db.Model):
     __tablename__ = "politicalcandidates"
 
     candidate_id = db.Column(db.Integer,
-                            primary_key=True 
+                            primary_key=True, 
                             autoincrement=True)
-    party_id = db.Column(db.Integer, nullable=False)
+    party_id = db.Column(db.Integer, db.ForeignKey('parties.party_id'), nullable=False)
+    candidate_name = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.String(400), nullable=False)
 
+    def __repr__(self):
+        """Return human-readable representation of Political Candidates Table"""
+        return f"<Political Candidates party_id={self.party_id} candidate_name={self.candidate_name}>"   
 
+##############################################################################
+#Helper Functions
 
+def connect_to_db(app):
+    """Connects the database to our Flask app"""
 
+    #Configures app to use our database
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///votings"
+    app.config["SQLALCHEMY_ECHO"] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
 
-    
-
-
-app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
-db.app = app
-db.init_app(app)
-
-db.create_all()
+if __name__ == "__main__":
+    # from server import app
+    connect_to_db(app)
--- server.py
+++ server.py
@@ -1,14 +1,38 @@
 import requests
+from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
+from model import connect_to_db, PollingCenter, Comment, User, Parties, PoliticalCandidates
 
-r = requests.get('https://www.googleapis.com/civicinfo/v2/voterinfo?address=990%20Jackson%20Street%20San%20Francisco%2C%20CA%2094133&key=AIzaSyCALOx3a43D4qa6l_2R9YJPAPF43A4NnjA')
+# r = requests.get('https://www.googleapis.com/civicinfo/v2/voterinfo?address=990%20Jackson%20Street%20San%20Francisco%2C%20CA%2094133&key=AIzaSyCALOx3a43D4qa6l_2R9YJPAPF43A4NnjA')
 
-voting_info = r.json()
+# voting_info = r.json()
 
-polling_locations = voting_info['pollingLocations']
+# polling_locations = voting_info['pollingLocations']
 
-first_result = polling_locations[0]
+# first_result = polling_locations[0]
 
-for polling_address in first_result['address']:
-    print fir
+#####################################################################################################################################################################################
+app = Flask(__name__)
+app.secret_key = "SECRETKEY"
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def homepage():
+    """Shows the homepage"""
+    return render_template("index.html")
+
+# @app.route('/nearme', methods=['GET'])
+# def address_form():
+#     """Show form for user to type in address or get nearby location"""
+
+#     return render_template("address_form.html")
+
+if __name__ == "__main__":
+    app.debug = True
+
+    connect_to_db(app)
+
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")

92394620ee5295d61ecc4869b418c0134f344076
kmchuc <vagrant@vagrant.vm>
2020-03-04 23:19:54 +0000

    seeded political party table

--- model.py
+++ model.py
@@ -136,7 +136,7 @@ def connect_to_db(app):
 
     #Configures app to use our database
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///votings"
-    app.config["SQLALCHEMY_ECHO"] = False
+    app.config["SQLALCHEMY_ECHO"] = True
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
--- seed.py
+++ seed.py
@@ -23,8 +23,6 @@ def load_parties():
     
     # Once seeding is done, should commit our work
     db.session.commit()
-    
-
 
 def set_val_user_id():
     """Sets the value for next user in database"""
@@ -35,11 +33,12 @@ def set_val_user_id():
 
     # Set the value for the next user_id to be max_id +1
     query = "SELECT setval('users_user_id_seq', :new_id)"
-    db.session.execute(query, {'new_id' + 1})
+    db.session.execute(query, {'new_id': max_id + 1})
     db.session.commit()
 
 if __name__ == "__main__":
     connect_to_db(app)
     db.create_all()
 
+    load_parties()
     set_val_user_id()
